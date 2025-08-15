83585541b3204cc9f562b0361c088659b8759510
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2025-08-15 23:51:22 +0530

    added deployed model

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -78,13 +78,16 @@ interface UserWatchlist {
 }
 export const getRecommendationsFromML = async (title: string) => {
   try {
-    const response = await fetch("http://127.0.0.1:8080/recommend", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ movie_title: title }),
-    });
+    const response = await fetch(
+      "https://cinephile-model.onrender.com/recommend",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ movie_title: title }),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
